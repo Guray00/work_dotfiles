85359d503954aff07d906341aac1d8fe7db21c70
Marco Lampis <30523962+Guray00@users.noreply.github.com>
2025-03-04 09:19:17 +0000

    initial

---
+++ scripts/code_default_editor.sh
@@ -0,0 +1,33 @@
+code_as_default_text_editor() {
+  local extensions=(
+    ".c"
+    ".cpp"
+    ".js"
+    ".jsx"
+    ".ts"
+    ".tsx"
+    ".json"
+    ".md"
+    ".sql"
+    ".html"
+    ".css"
+    ".scss"
+    ".sass"
+    ".py"
+    ".sum"
+    ".rs"
+    ".go"
+    ".sh"
+    ".log"
+    ".toml"
+    ".yml"
+    ".yaml"
+    "public.plain-text"
+    "public.unix-executable"
+    "public.data"
+  )
+
+  for ext in $extensions; do
+    duti -s com.microsoft.VSCode $ext all
+  done
+}
